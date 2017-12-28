b75a502b1da733b36adfb5e7e11892e54b3179f0
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-29 00:01:58 +0530

    Create Address.java

---
+++ com/app/model/Address.java
@@ -0,0 +1,43 @@
+package com.app.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Address_OTO")
+public class Address {
+	
+	private int addrId;
+	private String location;
+
+	@Id
+	//@Column(name="ADDRID")
+	public int getAddrId() {
+		return addrId;
+	}
+	public void setAddrId(int addrId) {
+		this.addrId = addrId;
+	}
+	
+	//@Column(name="LOCATION")
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	@Override
+	public String toString() {
+		return "Address [addrId=" + addrId + ", location=" + location + "]";
+	}
+
+	
+	
+}
